f5bf9b2cb4723e976ffef296c64268998b7d8fd0
iamabdoulayes <abdoulayesow835@gmail.com>
2026-06-15 18:03:37 -0500

    Fix backend URL path

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 const config = {
-  backendUrl: "http://devops-challenge-alb-2136896932.us-east-2.elb.amazonaws.com/api"
+  backendUrl: "http://devops-challenge-alb-2136896932.us-east-2.elb.amazonaws.com/api/id"
 };
 export default config;
